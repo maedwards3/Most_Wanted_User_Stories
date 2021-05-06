b43466e85a6d07fc85b3d78c5f684761ff94c6b8
Austin Flatt <austindflatt@gmail.com>
2021-05-06 15:54:26 -0400

    modified index and added js to app

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 "use strict";
+
+function searchByName(){
+    // Grabbing the values from our nameForm form and inputs.
+    let firstNameInput = document.forms['nameForm']['fname'].value;
+    let lastNameInput = document.forms['nameForm']['lname'].value;
+
+    // "people" is coming from the data.js file. We have access to it within this JavaScript file.
+    let filteredPeople = people.filter(function (person) {
+        if(person.firstName === firstNameInput && person.lastName === lastNameInput){
+            return true;
+        }
+        return false;
+    });
+    
+    // Rather than console logging, you need to append the filteredPeople to a table.
+    if(filteredPeople.length > 0){
+        console.log(filteredPeople);
+    }else{
+        console.log('Sorry, looks like there is no one with that name.');
+    }
+}
+
+function renderTable(){
+    let concat = ''
+    people.map(function (person){
+        return concat += `<tr>
+        <td>${person.id}</td>
+        <td>${person.firstName}</td>
+        </tr>`
+    })
+    document.getElementById("body").innerHTML = concat
+}
+
+renderTable()
+
